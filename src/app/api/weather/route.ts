4df41d6a0308c279c6f6d5c570da4bf14cbd7d14
Artur Jordão <artur.dev@hotmail.com>
2023-11-27 22:24:12 -0300

    test

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,7 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export async function GET(request: any) {
-	const { searchParams } = new URL(request.url)
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	const { searchParams } = new URL(req.url)
 	const address = searchParams.get('address')
 	const latitude = searchParams.get('lat')
 	const longitude = searchParams.get('lon')
@@ -11,13 +14,19 @@ export async function GET(request: any) {
 		url =
 			'https://api.openweathermap.org/data/2.5/weather?q=' +
 			address +
-			'&lang=pt_br'+
+			'&lang=pt_br' +
 			'&appid=' +
 			'83c6abdcf2161f1435149e9cff3adbc9'
 	} else {
 		url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=pt_br&appid=83c6abdcf2161f1435149e9cff3adbc9`
 	}
-	const res = await fetch(url)
-	const data = await res.json()
-	return NextResponse.json({ data })
+
+	try {
+		const response = await fetch(url)
+		const data = await response.json()
+		res.json({ data })
+	} catch (error) {
+		console.error('Error fetching data:', error)
+		res.status(500).send('Internal Server Error')
+	}
 }
