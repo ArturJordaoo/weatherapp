83794aab78134f13ce9e41369579311d3174d14c
Artur Jordão <artur.dev@hotmail.com>
2024-12-09 21:18:34 -0300

    update 1.01

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,23 +1,47 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: any) {
-	const { searchParams } = new URL(request.url)
-	const address = searchParams.get('address')
-	const latitude = searchParams.get('lat')
-	const longitude = searchParams.get('lon')
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const address = searchParams.get('address');
+  const latitude = searchParams.get('lat');
+  const longitude = searchParams.get('lon');
 
-	let url = ''
-	if (address) {
-		url =
-			'https://api.openweathermap.org/data/2.5/weather?q=' +
-			address +
-			'&lang=pt_br' +
-			'&appid=' +
-			'83c6abdcf2161f1435149e9cff3adbc9'
-	} else {
-		url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=pt_br&appid=83c6abdcf2161f1435149e9cff3adbc9`
-	}
-	const res = await fetch(url)
-	const data = await res.json()
-	return NextResponse.json({ data })
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+  if (!apiKey) {
+    return NextResponse.json({ error: 'API key not found' }, { status: 500 });
+  }
+
+  let url = '';
+  if (address) {
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${address}&lang=pt_br&appid=${apiKey}`;
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&lang=pt_br&appid=${apiKey}`;
+  }
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    // Retorna a previsão das próximas 5 horas
+    const hourlyForecast = data.list.slice(0, 5).map((forecast: any) => ({
+      temp: forecast.main.temp,
+      dt: forecast.dt,
+    }));
+
+    // Formata a resposta para incluir dados gerais e previsão
+    return NextResponse.json({
+      data: {
+        ...data.city,
+        main: data.list[0].main,
+        weather: data.list[0].weather,
+        wind: data.list[0].wind,
+        hourly: hourlyForecast,
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch weather data' },
+      { status: 500 },
+    );
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,126 +1,158 @@
 /* Inside your React component file */
-'use client'
+'use client';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from './page.module.css';
-import Image from 'next/image';
 
 interface WeatherData {
   main: {
     temp: number;
+    feels_like: number;
   };
   weather: {
     description: string;
-    icon: string; 
+    icon: string;
   }[];
+  wind: {
+    speed: number;
+  };
   name: string;
+  hourly: {
+    temp: number;
+    dt: number;
+  }[];
 }
 
 function getCurrentDate() {
-	const currentDate = new Date()
-	const options = { month: 'long' } as Intl.DateTimeFormatOptions
-	const monthName = currentDate.toLocaleString('pt-br', options)
-	const date = new Date().getDate() + ', ' + monthName
-	return date
+  const currentDate = new Date();
+  const options = { month: 'long' } as Intl.DateTimeFormatOptions;
+  const monthName = currentDate.toLocaleString('pt-br', options);
+  const date = new Date().getDate() + ', ' + monthName;
+  return date;
 }
 
 export default function Home() {
-	const date = getCurrentDate()
-	const [weatherData, setWeatherData] = useState<WeatherData | null>(null)
-	const [city, setCity] = useState('recife')
+  const date = getCurrentDate();
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [city, setCity] = useState('recife');
+  const [showDetails, setShowDetails] = useState(false);
 
-	async function fetchData(cityName: string): Promise<void> {
-		try {
-			const response = await fetch('/api/weather?address=' + cityName)
-			const jsonData: { data: WeatherData } = await response.json()
-			setWeatherData(jsonData.data)
-		} catch (error) {
-			console.error('Error fetching data:', error)
-		}
-	}
+  async function fetchData(cityName: string): Promise<void> {
+    try {
+      const response = await fetch('/api/weather?address=' + cityName);
+      const jsonData: { data: WeatherData } = await response.json();
+      setWeatherData(jsonData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
-	async function fetchDataByCoordinates(
-		latitude: number,
-		longitude: number,
-	): Promise<void> {
-		try {
-			const response = await fetch(
-				`/api/weather?lat=${latitude}&lon=${longitude}`,
-			)
-			const jsonData: { data: WeatherData } = await response.json()
-			setWeatherData(jsonData.data)
-		} catch (error) {
-			console.error('Error fetching data:', error)
-		}
-	}
+  async function fetchDataByCoordinates(
+    latitude: number,
+    longitude: number,
+  ): Promise<void> {
+    try {
+      const response = await fetch(
+        `/api/weather?lat=${latitude}&lon=${longitude}`,
+      );
+      const jsonData: { data: WeatherData } = await response.json();
+      setWeatherData(jsonData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
-	useEffect(() => {
-		if ('geolocation' in navigator) {
-			navigator.geolocation.getCurrentPosition(
-				(position) => {
-					const { latitude, longitude } = position.coords
-					fetchDataByCoordinates(latitude, longitude)
-				},
-				(error) => {
-					console.error('Error getting geolocation:', error)
-				},
-			)
-		}
-	}, [])
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          fetchDataByCoordinates(latitude, longitude);
+        },
+        (error) => {
+          console.error('Error getting geolocation:', error);
+        },
+      );
+    }
+  }, []);
 
-	return (
-		<main className={styles.main}>
-			<article className={styles.widget}>
-				<div className="components">
-					<form
-						className={styles.weatherLocation}
-						onSubmit={(e) => {
-							e.preventDefault()
-							fetchData(city)
-						}}
-					>
-						<input
-							className={styles.input_field}
-							placeholder="Nome Da Cidade"
-							type="text"
-							id="cityName"
-							name="cityName"
-							onChange={(e) => setCity(e.target.value)}
-						/>
-						<button className={styles.search_button} type="submit">
-							Search
-						</button>
-					</form>
-					{weatherData && weatherData.weather && weatherData.weather[0] ? (
-						<>
-							<div className={styles.icon_and_weatherInfo}>
-								<div className={styles.weatherIcon}>
-								<Image
-  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-  alt='/'
-  width='100'
-  height='100'
-/>
-								</div>
-								<div className={styles.weatherInfo}>
-									<div className={styles.temperature}>
-										<span>
-											{(weatherData?.main?.temp - 273.5).toFixed(1) +
-												String.fromCharCode(176)}
-										</span>
-									</div>
-									<div className={styles.weatherCondition}>
-										{weatherData?.weather[0]?.description?.toUpperCase()}
-									</div>
-								</div>
-							</div>
-							<div className={styles.place}>{weatherData?.name}</div>
-							<div className={styles.date}>{date}</div>
-						</>
-					) : (
-						<div className={styles.place}>Loading...</div>
-					)}
-				</div>
-			</article>
-		</main>
-	)
+  return (
+    <main className={styles.main}>
+      <article className={styles.widget}>
+        <div className="components">
+          <form
+            className={styles.weatherLocation}
+            onSubmit={(e) => {
+              e.preventDefault();
+              fetchData(city);
+            }}
+          >
+            <input
+              className={styles.input_field}
+              placeholder="Nome Da Cidade"
+              type="text"
+              id="cityName"
+              name="cityName"
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button className={styles.search_button} type="submit">
+              Search
+            </button>
+          </form>
+          {weatherData && weatherData.weather && weatherData.weather[0] ? (
+            <>
+              <div className={styles.icon_and_weatherInfo}>
+                <div className={styles.weatherIcon}>
+                  <Image
+                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                    alt="/"
+                    width="100"
+                    height="100"
+                  />
+                </div>
+                <div className={styles.weatherInfo}>
+                  <div className={styles.temperature}>
+                    <span>
+                      {(weatherData.main.temp - 273.5).toFixed(1) + '°'}
+                    </span>
+                  </div>
+                  <div className={styles.weatherCondition}>
+                    {weatherData.weather[0].description.toUpperCase()}
+                  </div>
+                </div>
+              </div>
+              <div className={styles.place}>{weatherData.name}</div>
+              <div className={styles.date}>{date}</div>
+              <button
+                className={styles.details_button}
+                onClick={() => setShowDetails(!showDetails)}
+              >
+                + Detalhes
+              </button>
+              {showDetails && (
+                <div className={styles.detailsDropdown}>
+                  <p>
+                    Sensação Térmica:{' '}
+                    {(weatherData.main.feels_like - 273.5).toFixed(1)}°
+                  </p>
+                  <p>Vento: {weatherData.wind.speed} m/s</p>
+                  <p>Previsão para próximas horas:</p>
+                  <ul>
+                    {weatherData.hourly?.map((hour) => (
+                      <li key={hour.dt}>
+                        {new Date(hour.dt * 1000).toLocaleTimeString()} -{' '}
+                        {(hour.temp - 273.5).toFixed(1)}°
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className={styles.place}>Loading...</div>
+          )}
+        </div>
+      </article>
+    </main>
+  );
 }
